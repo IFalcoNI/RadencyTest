5c907ef02e0ea4296e5d4bfc9a1b8e5045095b2f
FalcoN <48328425+IFalcoNI@users.noreply.github.com>
2022-09-26 23:24:45 +0300

    some fixes

--- main.js
+++ main.js
@@ -34,14 +34,16 @@ function loadGrain(levels) {
       }
     } else {
       end = indexOfLower[indexOfLower.length - 1];
-      if (end >= indexOfHighest[indexOfHighest.length - 1]) {
+      if (end > indexOfHighest[indexOfHighest.length - 1]) {
         start = indexOfHighest[indexOfHighest.length - 1];
       } else {
         start = indexOfHighest[0];
       }
     }
     for (let j = start + 1; j < end; j++) {
-      sortedLevels.splice(sortedLevels.indexOf(levels[j]), 1)
+      if (sortedLevels.indexOf(levels[j]) !== -1) {
+        sortedLevels.splice(sortedLevels.indexOf(levels[j]), 1);
+      }
       if (indexOfHighest[indexOfHighest.length - 1] >= indexOfLower[indexOfLower.length - 1]) {
         if (levels[j] < levels[start]) {
           freeSpaces += levels[start] - levels[j];
@@ -54,17 +56,18 @@ function loadGrain(levels) {
         }
       }
     }
+
     sortedLevels.shift();
     indexOfHighest = [];
     indexOfLower = [];
-
     if (sortedLevels.length < 2) {
       break;
     }
   }
   return freeSpaces + ' free spaces';
 }
-console.log(loadGrain([42, 44, 49, 2, 24, 40, 4, 28, 13, 22, 16, 23])); //96
+console.log(loadGrain([32, 12, 37, 15, 17, 15, 24, 26, 44, 34, 28, 25, 2, 8, 17, 41, 26, 17, 29, 27, 27, 28])); //96
+// console.log(loadGrain([42, 44, 49, 2, 24, 40, 4, 28, 13, 22, 16, 23])); //96
 // console.log(loadGrain([10, 45, 44, 41, 35, 28, 12, 35, 8, 40])); //82
 // console.log(loadGrain([4, 1, 3])); //2
 // console.log(loadGrain([2, 1, 5, 2, 7, 4, 10])); //7
