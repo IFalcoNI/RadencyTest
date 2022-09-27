1574d3083132b42b76c7de682fa1836feb65d1a3
FalcoN <48328425+IFalcoNI@users.noreply.github.com>
2022-09-27 17:21:33 +0300

    some fixes

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 function loadGrain(levels) {
-  if (levels.length < 3) {
-    return 0 + ' free spaces';
-  }
   let freeSpaces = 0;
   let uniqueLevels = [...levels];
   let sortedLevels = uniqueLevels.sort((a, b) => {
@@ -12,7 +9,7 @@ function loadGrain(levels) {
   let indexOfLower = [];
   let start = 0;
   let end = 0;
-  for (; ;) {
+  do {
 
     levels.filter(function (arr, index) {
       if (arr === sortedLevels[0]) {
@@ -40,7 +37,7 @@ function loadGrain(levels) {
         start = indexOfHighest[0];
       }
     }
-    for (let j = start + 1; j < end; j++) {
+    for (let j = start; j < end; j++) {
       if (sortedLevels.indexOf(levels[j]) !== -1) {
         sortedLevels.splice(sortedLevels.indexOf(levels[j]), 1);
       }
@@ -56,26 +53,29 @@ function loadGrain(levels) {
         }
       }
     }
-
-    sortedLevels.shift();
+    // console.log(levels[start]);
+    // console.log(levels[end]);
+    // console.log(levels);
+    // console.log(sortedLevels);
     indexOfHighest = [];
     indexOfLower = [];
     if (sortedLevels.length < 2) {
       break;
     }
-  }
+  } while (true)
   return freeSpaces + ' free spaces';
 }
-console.log(loadGrain([32, 12, 37, 15, 17, 15, 24, 26, 44, 34, 28, 25, 2, 8, 17, 41, 26, 17, 29, 27, 27, 28])); //257
+
+// console.log(loadGrain([45, 10, 43, 49, 3, 37, 35, 23, 40, 37, 4, 5, 36, 3, 24, 45, 42, 29, 21, 32])); //299
+// console.log(loadGrain([32, 12, 37, 15, 17, 15, 24, 26, 44, 34, 28, 25, 2, 8, 17, 41, 26, 17, 29, 27, 27, 28])); //257
 // console.log(loadGrain([42, 44, 49, 2, 24, 40, 4, 28, 13, 22, 16, 23])); //96
 // console.log(loadGrain([10, 45, 44, 41, 35, 28, 12, 35, 8, 40])); //82
-
 // console.log(loadGrain([4, 1, 3])); //2
 // console.log(loadGrain([2, 1, 5, 2, 7, 4, 10])); //7
 // console.log(loadGrain([2, 0, 1, 5, 2, 7])); //6
-// console.log(loadGrain([2, 4, 2])); //0
-// console.log(loadGrain([7, 4])); //0
-// console.log(loadGrain([])); //0
+console.log(loadGrain([2, 4, 2])); //0
+console.log(loadGrain([7, 4])); //0
+console.log(loadGrain([])); //0
 // console.log(loadGrain([4, 0, 1, 3])); //5
 // console.log(loadGrain([3, 1, 2, 1, 1, 3, 2])); //7
 // console.log(loadGrain([9, 1, 4, 1, 6, 4, 10])); //29
